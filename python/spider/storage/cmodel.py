3fbf13a7b90d6d83aea9807ea2a6ca3d13f40231
polly <wruibo@gmail.com>
2017-05-12 00:41:55 +0800

    for backup

--- python/spider/storage/cmodel.py
+++ python/spider/storage/cmodel.py
@@ -14,20 +14,26 @@ class MetaModel(type):
             if issubclass(value.__class__, Table):
                 for field in value.fields:
                     fields[field.name] = field
-                    attrs[field.name] = field.default.default()
+        #set fields declare and items
         attrs['fields'] = fields
 
         return type.__new__(cls, name, bases, attrs)
+
+    def __init__(self, *args, **kwargs):
+        pass
 
 class Model(dict):
     __metaclass__ = MetaModel
 
     def __init__(self, **kwargs):
+        for name, field in self.fields.items():
+            self[name] = field.default.default()
+
         for key in kwargs.keys():
             self[key] = kwargs.get(key, self.fields[key].default.default())
 
     def __getattr__(self, name):
-        return self[name]
+        return self.get(name, self.fields[name].default.default())
 
     def __setattr__(self, name, value):
         self[name] = value
