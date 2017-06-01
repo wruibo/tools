172e7e0e5b9de136796b50682d211ff7588535e0
polly <wangruibo@oak-inc.com>
2017-06-01 21:05:02 +0800

    add new codes

--- python/security/algo/perf.py
+++ python/security/algo/perf.py
@@ -1,17 +1,15 @@
 '''
     performance attribution algorithms
 '''
+import csv
 
 
-class Benchmark:
+class BLoader:
     '''
-        benchmark information for brinson analysis
+        benchmark data loader
     '''
     def __init__(self):
-        #benchmark index, date->index value
-        self.index ={}
-        #industry index, code->{date->index value}
-        self.industry = {}
+        pass
 
     def load(self, source):
         '''
@@ -56,28 +54,96 @@ class Benchmark:
         pass
 
 
+class PLoader:
+    '''
+        portfolio data loader
+    '''
+    def __init__(self):
+        pass
+
+
+class Benchmark:
+    '''
+        benchmark information for brinson analysis
+    '''
+    def __init__(self):
+        #benchmark index: date->[open-price, close-price]
+        self.bindexs = None
+        #industry index: code->{date->[open-price, close-price]}
+        self.iindexs = None
+        #industry weight in benchmark: industry code->weight
+        self.iweights = None
+        #stock industries in benchmark: stock code->industry code
+        self.sindustries = None
+
+
+    def load(self, loader):
+        '''
+            load benchmark data for brinson
+        :param loader:
+        :return:
+        '''
+        self.bindexs, self.iindexs, self.iweights, self.sindustries = loader.load()
+        return self
+
+
 class Portfolio:
     '''
         portfolio information for analysis
     '''
     def __init__(self):
-        pass
+        #daily equity holdings after daily transaction ends: date->{code->[holds, open-price, close-price]}
+        self.holdings = None
 
+    def load(self, loader):
+        '''
+            load portfolio data for brinson
+        :param loader:
+        :return:
+        '''
+        self.holdings = loader.load()
+        return self
 
 class Brinson:
     '''
         brinson performance attribution algorithm
     '''
     def __init__(self):
-        pass
+        #benchmark for portfolio
+        self.benchmark = None
+        #portfolio to be attributed
+        self.portfolio = None
 
-    def init(self, benchmark, industry, portfolio):
-        # benchmark for portfolio
+    def init(self, benchmark, portfolio):
+        #benchmark for portfolio
         self.benchmark = benchmark
-        # portfolio to be attributed
+        #portfolio to be attributed
         self.portfolio = portfolio
 
         return self
 
     def analyse(self):
-        pass
+        '''
+            brinson analyse for portfolio with specified benchmark
+        :return:
+        '''
+        attribution_results = []
+        for date, stocks in self.portfolio.holdings:
+            #compute benchmark return as q1
+            q1 = (self.benchmark.bindexs[date][1] - self.benchmark.bindexs[date][0]) / self.benchmark.bindexs[date][0]
+
+            #compute portfolio return as q4
+            openv, closev = 0, 0
+            for count, open, close in stocks.values():
+                 openv += count*open
+                 closev += count*close
+            q4 = (closev - openv) / openv
+
+            #compute industry selection return
+            for code, holds in stocks.items():
+                pass
+
+            #compute stock selection return
+            #compute interaction return
+
+        return attribution_results
